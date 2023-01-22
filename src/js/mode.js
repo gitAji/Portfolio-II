871e5db482e9c8865810a850362a09588d940390
gitAji <89026824+gitAji@users.noreply.github.com>
2023-01-22 01:09:32 +0100

    Mode issue fixed

---
+++ src/js/mode.js
@@ -0,0 +1,11 @@
+  //from tailwind darkmode 
+  // It's best to inline this in `head` to avoid FOUCs (flash of unstyled content) when changing pages or themes
+  if (
+    localStorage.getItem('color-theme') === 'dark' ||
+    (!('color-theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
